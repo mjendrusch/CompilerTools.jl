4e4b1c8ad53b33bf7c005ef9e59cc555dcfbbf6f
Michael Jendrusch <Jendrusch@stud.uni-heidelberg.de>
2017-12-04 05:54:55 +0100

    added :inert

--- src/ast_walk.jl
+++ src/ast_walk.jl
@@ -430,6 +430,10 @@ function from_expr_helper(ast::Expr,
         for i = 1:length(args)
             args[i] = from_expr(args[i], depth, callback, cbdata, top_level_number, false, read)
         end
+    elseif head == :inert
+        for i = 1:length(args)
+            args[i] = from_expr(args[i], depth, callback, cbdata, top_level_number, false, read)
+        end
     else
         throw(string("CompilerTools.AstWalker.from_expr: unknown Expr head :", head, " in ", ast))
     end
